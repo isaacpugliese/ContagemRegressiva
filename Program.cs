9ebd5643ac81f2279dc7b3c80595fdcc7771c213
Isaac Pugliese <isaacpugliese2010@gmail.com>
2020-09-18 22:08:34 -0300

    contagem finalizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace contagemregressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Precione qualquer tecla para iniciar");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.WriteLine("FOGO!!"); 
+            
+        }
+    }
+}
